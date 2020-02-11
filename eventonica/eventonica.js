2b379a35df223d36e57651c325fd626e494dbc2d
Caden <cadence1547@gmail.com>
2020-02-10 23:42:09 -0800

    add css and modify jQuery

--- eventonica/eventonica.js
+++ eventonica/eventonica.js
@@ -7,31 +7,43 @@ class EventRecommender {
     this.users = [];
     }
 
-    addEvent(newEvent) {
-      this.events.push(newEvent);
+    addEvent(eventName,id,date,category) {
+      let addEventToList=new Event(eventName,id,date,category)
+      this.events.push(addEventToList);
     }
 
-    addUser(newUser) {
-        this.users.push(newUser);
+    addUser(newUser,id) {
+        let addToThisUser=new User(newUser,id)
+       
+        this.users.push(addToThisUser);
     }
-
-    saveUserEvent(user, event){
-    //    user.push(event)
+   
+    saveUserEvent(user, eventId){
+       
        user.addPersonEvent(event)
 
        // Allow users to save events to a personal Events array.
     }
 
-    deleteUser(user) {
-      this.users.splice(this.users.indexOf(user),1);
-  return this.users
+    deleteUser(id) {
+      let theRestOfTheUsers=this.users.filter(user =>user.id !==id);
+      this.users=theRestOfTheUsers;
+  
+//         // console.log(event.users, )
+//         console.log(`before splicing: :${JSON.stringify(this.users)}`);
+//       this.users.splice(this.users.indexOf(user),1);
+//       console.log(`after splicing: ${JSON.stringify(this.users)}`);
+//   return this.users
+  
      
       
     }
    
-    deleteEvent(event) {
-        this.events.splice(this.events.indexOf(event),1);
-  return this.events
+    deleteEvent(id) {
+        let theRestOfTheEvent=this.events.filter(event=>event.id !==id);
+        this.events=theRestOfTheEvent;
+//         this.events.splice(this.events.indexOf(event),1);
+//   return this.events
     }
 
     findEventsByDate(dates){
@@ -56,8 +68,9 @@ class EventRecommender {
 }
 
 class User{
-    constructor(newUser){
+    constructor(newUser,id){
        this.newUser=newUser;
+        this.id=id;
         this.personalEvent=[]
     }
     addPersonEvent(event){
@@ -66,8 +79,9 @@ class User{
     
 }
 class Event{
-    constructor (eventName,date,category){
+    constructor (eventName,id,date,category){
         this.eventName=eventName;
+        this.id=id;
          this.date=date;
          this.category=category;
     }  
@@ -81,18 +95,25 @@ class Event{
 
 
 
-const event1= new Event("happy Hour After Work","Tuesday", "happy hour");
-const event2 =new Event("Weekend Pinic","Sunday", "happy weekend");
-const user1=new User("Bob");
-const user2=new User("kate");
+// const event1= new Event("happy Hour After Work","Tuesday", "happy hour");
+// const event2 =new Event("Weekend Pinic","Sunday", "happy weekend");
+const user1=new User("Bob", "1");
+// const user2=new User("kate");
 let recommandation =new EventRecommender();
 recommandation.addUser(user1);
-recommandation.addUser(user2);
-recommandation.addEvent(event1);
-recommandation.addEvent(event2);
-user1.addPersonEvent("Studying");
+// recommandation.addUser(user2);
+// recommandation.addEvent(event1);
+// recommandation.addEvent(event2);
+// user1.addPersonEvent("Studying");
+// recommandation.deleteUser(user1)
+console.log("all users: "+recommandation.users)
+
+
+
+//all the events that is under the EventRecommender
 // console.log(recommandation.events)
-console.log(recommandation.users[0])
+// console.log(recommandation.users[0])
+// console.log(`recommandation.deleteUser(user1): ${JSON.stringify(recommandation.deleteUser(user1))}`);
 // testing to see if search by date and category works
 // console.log(recommandation.events);
 // console.log(recommandation.users);
@@ -103,12 +124,3 @@ if (typeof module != 'undefined'){
     module.exports = { EventRecommender, User,  Event} 
 }
 
-class classRoom {
-    constructor(){
-    this.name=name;
-    this.name1={};
-    }
-}
-
-let bigClass=new classRoom("happy")
-console.log(bigClass)
--- eventonica/jQuery-scripts.js
+++ eventonica/jQuery-scripts.js
@@ -1,5 +1,5 @@
 $(document).ready( () => {
-    // const eventRecommender = new EventRecommender(); 
+    const eventRecommender = new EventRecommender(); 
     // console.log(recommandation)
     // let html="";
     // $.each(recommandation.users, function(index,item){
@@ -8,37 +8,111 @@ $(document).ready( () => {
     // $("#all-users").html(html);
    
     $("#button").click(function(e){
+         let html="";
         e.preventDefault();
         let addId=$("#add-user-id").val();
-    let addUser=$("#add-user-name").val();
-   
-        $("#all-users").append("<li>User ID: "  + addId + ": " + "User Name: "+ addUser +"</li>")
+    let addNewUser=$("#add-user-name").val();
+        eventRecommender.addUser(addNewUser,addId);
+
+        $.each(eventRecommender.users, function(index,item){
+          html+=`<li>${item.newUser}</li>`
+    });
+    $("#all-users").html(html);
+        // console.log(addUserToList);
+        // $("#all-users").append("<li>"+ addUserToList+"</li>")
+        // $("#all-users").append("<li>User ID: "  + addId + ": " + "User Name: "+ addUser +"</li>")
         // $("#all-users").html(addUser)
        
     })
     
-
+// detele user block 
     $("#deleteBtn").click(function(e){
         e.preventDefault();
-
-        let allUserText=$(all-users).text();
-        $("#allUser li").each(function(){
-            if($("#add-user-id").val()===$("#delete-user-id").val()){
-                $("#all-users li").remove();
-            }
-        })
+        let html="";
+        let valueOfdeleteUserId=$("#delete-user-id").val();
+        eventRecommender.deleteUser(valueOfdeleteUserId);
+        $.each(eventRecommender.users, function(index,item){
+            html+=`<li>${item.newUser}</li>`
+      });
+      $("#all-users").html(html);
+        // // let allUserText=$(all-users).text();
+        // $("#all-users li").each(function(){
+        //     if($("#all-users li").text===$("#delete-user-id").val()){
+        //         $("#all-users li").remove();
+        //     }
+        // })
      
     })
 
-    // Event Management 
+    // Add Event 
      $("#eventBtn").click(function(e){
          e.preventDefault();
+         let html="";
          let eventId=$("#add-event-id").val();
           let eventName=$("#add-event-name").val();
-          $("#eventList").append("<li>Event Id:"+eventId+ ",Event Name:" + eventName+"</li>")
+          let eventDate=$("#add-event-date").val();
+         let  eventCategory=$("#add-event-category").val();
+           eventRecommender.addEvent(eventName,eventId,eventDate,eventCategory)
+            $.each(eventRecommender.events, function(index,item){
+                html+=`<li>Event: ${item.eventName}<br> Date:${item.date}-Category:${item.category}</li>`
+          });
+          $("#eventList").html(html);
+        
+        //   $("#eventList").append("<li>Event Id:"+eventId+ ",Event Name:" + eventName+"</li>")
           
      })
 
+//Delete event 
+$("#deleteEventBtn").click(function(e){
+    e.preventDefault();
+    let html="";
+    let deleteEventId=$("#delete-event-id").val();
+    eventRecommender.deleteEvent(deleteEventId);
+    $.each(eventRecommender.events, function(index,item){
+        html+=`<li>${item.eventName}</li>`
+  });
+  $("#eventList").html(html);
 
+});
+
+// Search Event By date
+$("#dateBtn").click(function(e){
+    e.preventDefault();
+    let html="";
+    let searchByDate=$("#date-search-id").val();
+     eventRecommender.findEventsByDate(searchByDate);
+     $.each(eventRecommender.events, function(index,item){
+         if(searchByDate===item.date){
+        html+=`<li>${item.eventName}</li>`
+         }
+  });
+  $("#result").html(html);
+
+});
+
+// Search By Category
+$("#categoryBtn").click(function(e){
+    e.preventDefault();
+    let html="";
+    let searchByCategory=$("#category-search-id").val();
+     eventRecommender.findEventsByDate(searchByCategory);
+     $.each(eventRecommender.events, function(index,item){
+         if(searchByCategory===item.category){
+        html+=`<li>${item.eventName}</li>`
+         }
+  });
+  $("#categoryResult").html(html);
+
+});
+
+ //Save Event for user: use nested loop
+// $("#categoryBtn").click(function(e){
+//     e.preventDefault();
+//     let html="";
+//      let userId=$("#save-user-id").val();
+//      let eventId=$("#save-event-id").val();
+//      eventRecommender.saveUserEvent(user,eventId);
+     
+// )}
 });
 
