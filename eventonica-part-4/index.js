6a1fc8f167baa7fd75a8257bc8e5893dab37b44c
Caden <cadence1547@gmail.com>
2020-02-18 23:53:40 -0800

    update missing file

---
+++ eventonica-part-4/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app=express()
+const port =3000;
+
+//testing
+// app.get("/", (req,res)=>res.send("Hello hooo"))
+// app.listen(port, ()=> console.log(`Example app listening on port ${port}`));
+
+// app.post("/", (req,res)=>{
+//     res.send('success')
+// })
+
+// app.put("/", function(req,res){
+//     res.send("send")
+// })
+
